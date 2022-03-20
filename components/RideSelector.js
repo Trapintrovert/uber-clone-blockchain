26eaf610bca10de477953496801b8356f7eb3741
Adedamola Adeniyi <mcneo062@gmail.com>
2022-03-20 11:34:43 +0100

    Map bug fixed

--- components/RideSelector.js
+++ components/RideSelector.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react'
+
 import uberX from '../assets/rides/uberX.png'
 import uberBlack from '../assets/rides/uberBlack.png'
 import uberBlackSuv from '../assets/rides/uberBlackSuv.png'
@@ -21,37 +23,30 @@ const style = {
     price: `mr-[-0.8rem]`,
   }
 
-  const carList = [
-    {
-        service: 'UberX',
-        iconUrl: uberX,
-        priceMultiplier: 1.5,
-    },
-    {
-        service: 'UberBlack',
-        iconUrl: uberBlack,
-        priceMultiplier: 1.5,
-    },
-    {
-        service: 'UberBlackSuv',
-        iconUrl: uberBlackSuv,
-        priceMultiplier: 1.5,
-    },
-    {
-        service: 'UberSelect',
-        iconUrl: uberSelect,
-        priceMultiplier: 1.5,
-    },
-    {
-        service: 'UberXL',
-        iconUrl: uberXL,
-        priceMultiplier: 1.5,
-    }
-  ]
+
   
   const basePrice = 1542
 
 const RideSelector = () => {
+    const [carList, setCarList] = useState([])
+
+    useEffect(() => {
+     ;(async () => {
+         try {
+             const response  = await fetch('/api/db/getRideTypes')
+             
+             const data = await response.json()
+
+             setCarList(data.data)
+
+
+         } catch (error) {
+             console.error(error)
+         }
+     })()
+    }, [])
+    
+
   return (
     <div className={style.wrapper}>
         <div className={style.title}>Choose a ride, or swipe up for more</div>
